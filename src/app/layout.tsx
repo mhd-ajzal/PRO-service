9f6d65ed0b6ad5cd65d25eea763b1adb20aaaa71
mhd-ajzal <ajzalmuhammed15@gmail.com>
2026-01-24 11:59:54 +0400

    Add Chatwoot widget

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { Inter as FontSans } from "next/font/google"; // Using Inter as default sans font
 import { cn } from "../components/ui/utils";
 import { WhatsAppButton } from "../components/WhatsAppButton";
+import Script from "next/script";
 
 const fontSans = FontSans({
     subsets: ["latin"],
@@ -27,6 +28,23 @@ export default function RootLayout({
             )}>
                 {children}
                 <WhatsAppButton />
+                <Script id="chatwoot-sdk" strategy="afterInteractive">
+                    {`
+                      (function(d,t) {
+                        var BASE_URL="http://chatwoot-twcoc0go8gg8cgsg0kwcog4o.72.62.191.145.sslip.io";
+                        var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+                        g.src=BASE_URL+"/packs/js/sdk.js";
+                        g.async = true;
+                        s.parentNode.insertBefore(g,s);
+                        g.onload=function(){
+                          window.chatwootSDK.run({
+                            websiteToken: 'LFQQNMEq872v3i3jwoRmZPZ9',
+                            baseUrl: BASE_URL
+                          })
+                        }
+                      })(document,"script");
+                    `}
+                </Script>
             </body>
         </html>
     );
